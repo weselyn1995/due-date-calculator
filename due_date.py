642941565796db889a974b67bf042307b83bd1dc
weselyn1995 <33514540+weselyn1995@users.noreply.github.com>
2025-03-15 13:17:12 +0100

    adding input type checks and respective tests

--- due_date.py
+++ due_date.py
@@ -2,4 +2,11 @@ from datetime import datetime
 
 
 def calculate_due_date(submit_datetime: datetime, turnaround_hours: int) -> datetime:
+
+    # Validate inputs
+    if not isinstance(submit_datetime, datetime):
+        raise TypeError('submit_datetime should be of type datetime')
+    if not isinstance(turnaround_hours, int):
+        raise TypeError('turnaround_hours should be an integer')
+    
     pass
--- test_due_date.py
+++ test_due_date.py
@@ -1,3 +1,4 @@
+import pytest
 from datetime import datetime
 from due_date import calculate_due_date
 
@@ -6,3 +7,11 @@ EXAMPLE_TURNAROUND_HOURS = 16
 
 def test_fuction_exists():
     calculate_due_date(EXAMPLE_REPORT_DATETIME, EXAMPLE_TURNAROUND_HOURS)
+
+def test_wrong_input_type_submit_datetime():
+    with pytest.raises(TypeError):
+        calculate_due_date(20240311, EXAMPLE_TURNAROUND_HOURS)
+
+def test_wrong_input_type_turnaround_hours():
+    with pytest.raises(TypeError):
+        calculate_due_date(EXAMPLE_REPORT_DATETIME, 'sixteen')
